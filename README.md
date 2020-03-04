886f805142a8252a0a5022744a1d4c2778b9feed
Carl Youngblood <carl@youngbloods.org>
2020-03-04 11:46:24 -0700

    Add development/deployment instructions

---
+++ README.md
@@ -0,0 +1,43 @@
+# Saltaires.org web site
+
+This is a Jekyll-based static web site that is used to manage the web presence
+of the Saltaires Barbershop Chorus of Salt Lake City.
+
+## Developing
+
+This assumes that [rvm](http://rvm.io/) and [nvm](https://github.com/nvm-sh/nvm)
+are installed in your environment. To set up your environment:
+```
+rvm install 2.4.0@saltaires --create
+rvm use 2.4.0@saltaires
+nvm install lts/carbon
+nvm use lts/carbon
+npm install -g bower
+bundle install
+bower install
+```
+To serve the site locally during development:
+```
+cd jekyll-site
+jekyll serve
+```
+Then browse to http://localhost:4000
+
+### Enabling seasonal functionality
+
+Note that site files ending in `.md` are in [Markdown
+format](https://www.markdownguide.org/basic-syntax/). To turn menu items on and
+off, edit `_config.yml`. To activate singing valentines orders, swap the
+contents of `valentines-hidden.md` with `valentines.md`, and edit `index.html`
+to uncomment promotional callout sections. To change events, edit `events.md`.
+
+Please note that the Valentines ordering functionality relies on a small
+lambda-based AWS API endpoint for Stripe server-side validation that is running
+in Carl Youngblood's AWS account. The cost of this endpoint is negligible and
+Carl is willing to host it indefinitely. It also depend's on our Stripe account,
+to which Mark Andromidas has access.
+
+## Deploying
+```
+./deploy.sh
+```
---
+++ deploy.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+super_echo() {
+  echo
+  echo "============================================================="
+  echo $1
+  echo "============================================================="
+  echo
+}
+
+CURRENT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Load RVM into a shell session *as a function*
+if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
+  # First try to load from a user install
+  source "$HOME/.rvm/scripts/rvm"
+elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
+  # Then try to load from a root install
+  source "/usr/local/rvm/scripts/rvm"
+else
+  printf "ERROR: An RVM installation was not found.\n"
+fi
+
+export NVM_DIR="$HOME/.nvm"
+. "$(brew --prefix nvm)/nvm.sh"
+
+cd $CURRENT_SCRIPT_DIR
+
+super_echo "Setting up nvm..."
+nvm use lts/carbon
+
+super_echo "Checking out master branch..."
+git checkout master
+
+super_echo "Installing bower dependencies..."
+npm install -g bower
+bower install
+
+super_echo "Building jekyll site..."
+cd jekyll-site
+jekyll build
+
+super_echo "Copying result to temp dir..."
+cd ..
+rm -rf /tmp/_site
+cp -r jekyll-site/_site /tmp
+
+super_echo "Cloning saltaires repo in /tmp..."
+cd /tmp
+git clone git@github.com:cayblood/saltaires.git
+cd saltaires
+
+super_echo "Checking out gh-pages branch..."
+git checkout gh-pages
+
+super_echo "Removing existing branch content..."
+rm -rf *
+mv /tmp/_site/* .
+echo -e "www.saltaires.org\n" > CNAME
+
+super_echo "Committing newly-built content in gh-pages branch..."
+git add .
+git commit -m "deploy"
+
+super_echo "Pushing changes to remote gh-pages branch..."
+git push -f origin gh-pages:gh-pages
+cd ..
+
+super_echo "Removing local copy..."
+rm -rf saltaires
